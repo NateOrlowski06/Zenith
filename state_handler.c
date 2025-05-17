4919eeb668179dbaa5c517b5b61855923e617e68
nateorlowski06 <nateorlowski@protonmail.com>
2025-05-16 21:53:23 -0400

    Implemented logic for state changes. TODO: determine threshold constants.

--- state_handler.c
+++ state_handler.c
@@ -1,22 +1,32 @@
 #include "state_handler.h"
 
-void handle_standby(const uint8_t *state, struct Altimeter altimeter){
-    
+void handle_standby(const uint8_t *state, struct Altimeter* altimeter){
+    //Create data buffer so we can log 5 seconds before flight starts
+
+    //Shifts the bit to the left by either 1 or zero each time
+    *state << (altimeter -> smooth_altitude > TAKEOFF_HEIGHT_THRESHOLD && 
+               altimeter -> smooth_velocity > TAKEOFF_VELO_THRESHOLD);
 }
 
-void handle_boost(const uint8_t *state, struct Altimeter altimeter){
+void handle_boost(const uint8_t *state, struct Altimeter* altimeter){
+    //Log data
 
+    *state << (altimeter -> max_velocity > altimeter -> smooth_velocity * MOTOR_BURNOUT_PERCENT);
 }
 
-void handle_coast(const uint8_t *state, struct Altimeter altimeter){
+void handle_coast(const uint8_t *state, struct Altimeter* altimeter){
+    //log data, perhaps altitude predictor algorithm, pretend to deploy ejection charges upon next state
 
+    *state << (altimeter -> max_height > altimeter -> height) && (altimeter -> smooth_velocity < FREEFALL_VELOCITY_THRESHOLD);
 }
 
-void handle_freefall(const uint8_t *state, struct Altimeter altimeter){
+void handle_freefall(const uint8_t *state, struct Altimeter* altimeter){
+    //Log data
 
+    *state << (altimeter -> height < LANDED_HEIGHT_THRESHOLD) || ((altimeter -> smooth_velocity) < LANDED_VELOCITY_THRESHOLD);
 }
 
-void handle_landed(const uint8_t *state, struct Altimeter altimeter){
-
+void handle_landed(const uint8_t *state, struct Altimeter* altimeter){
+    //Close data file
 }
 
--- state_handler.h
+++ state_handler.h
@@ -6,16 +6,22 @@
 #define FREEFALL 8
 #define LANDED 16
 
+#define TAKEOFF_HEIGHT_THRESHOLD 10
+#define TAKEOFF_VELO_THRESHOLD 5
+#define MOTOR_BURNOUT_PERCENT 0.95
+#define FREEFALL_VELOCITY_THRESHOLD -5
+#define LANDED_VELOCITY_THRESHOLD 5
+#define LANDED_HEIGHT_THRESHOLD 5
 
 inline void next_state(uint8_t *state){ *state << 1;}
 
-void handle_standby(const uint8_t *state, struct Altimeter altimeter);
-void handle_boost(const uint8_t *state, struct Altimeter altimeter);
-void handle_coast(const uint8_t *state, struct Altimeter altimeter);
-void handle_freefall(const uint8_t *state, struct Altimeter altimeter);
-void handle_landed(const uint8_t *state, struct Altimeter altimeter);
+void handle_standby(const uint8_t *state, struct Altimeter* altimeter);
+void handle_boost(const uint8_t *state, struct Altimeter* altimeter);
+void handle_coast(const uint8_t *state, struct Altimeter* altimeter);
+void handle_freefall(const uint8_t *state, struct Altimeter* altimeter);
+void handle_landed(const uint8_t *state, struct Altimeter* altimeter);
 
-void (*state_handlers[5])(const uint8_t *state, struct Altimeter altimeter) = {handle_standby,
+void (*state_handlers[5])(const uint8_t *state, struct Altimeter* altimeter) = {handle_standby,
                                                                        handle_boost,
                                                                        handle_coast,
                                                                        handle_freefall,
